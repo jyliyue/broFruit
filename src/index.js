fcc2ef83bd44bad302955acc9180b4d152a8c4d9
jiangliyue <772748611@qq.com>
2022-11-02 23:32:11 +0800

    图片资源

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 import * as PIXI from 'pixi.js'
 
+/* 加载游戏资源 */
+function loadAssets() {
+    const imgs = {
+        bg: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/f7f681ccd9a749dc89ed952765698c7e.png',
+        btn: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/8207f7c63986402284477874c02b3cb2.png',
+        board_bg: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/add924edf7574237b9df52c5616d0fad.png',
+        bianbian: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/157233c9fb24483ab8ee227c992c93b0.png',
+        apple: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/0b731d24301c4d7a9800a355e8989aeb.png',
+        bullet: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/fb74707d448749c0af159cddbdb1dbad.png',
+        warn: 'https://kuka-iretail-test.oss-cn-hangzhou.aliyuncs.com/salorder/6a97f5db4029410592a6782f3d3fda35.png'
+    }
+    PIXI.Assets.addBundle('assets', imgs);
+    return PIXI.Assets.loadBundle('assets');
+}
+
+/* 水果兄弟 */
 class BroFruit {
     constructor() {
         this.app = new PIXI.Application()
-        // this.assets = PIXI.Assets()
+        this.assets = null
     }
 
-    init = () => {
+    /* 初始化 */
+    init = async () => {
+        this.assets = await loadAssets()
         document.body.appendChild(this.app.view)
-        // this.loader.onComplete.add(() => {
-        //     console.log('complete loading')
-        // })
+        this.addBg()
+    }
+
+    /* 添加背景 */
+    addBg = async () => {
+        const bg = new PIXI.TilingSprite(
+            this.assets.bg,
+            800,
+            600
+        )
+        this.app.stage.addChild(bg)
     }
 }
 
